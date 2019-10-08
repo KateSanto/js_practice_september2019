3628d5318488f262c96846604856417be809486b
Kate Santo <katerine.santo@gmail.com>
2019-10-08 17:38:19 +0100

    Up to and including getMiddleCharacter functions working

--- challenges/week1.js
+++ challenges/week1.js
@@ -13,22 +13,29 @@ function generateInitials(firstName, lastName) {
 function addVAT(originalPrice, vatRate) {
   if (originalPrice === undefined) throw new Error("originalPrice is requied");
   if (vatRate === undefined) throw new Error("vatRate is required");
-let VAT = vatRate / 100
-let totalPrice = (VAT * originalPrice) + originalPrice;
-return totalPrice;
+  let VAT = vatRate / 100
+  let totalPrice = (VAT * originalPrice) + originalPrice;
+  return Math.round(totalPrice * 100) / 100;
 }
 
 function getSalePrice(originalPrice, reduction) {
   if (originalPrice === undefined) throw new Error("originalPrice is required");
   if (reduction === undefined) throw new Error("reduction is required");
-    let discount = reduction / 100
-    let discountedPrice = originalPrice - (discount * originalPrice);
-    return discountedPrice;
+  let discount = reduction / 100
+  let discountedPrice = originalPrice - (discount * originalPrice);
+  return Math.round(discountedPrice * 100) / 100;
 }
 
 function getMiddleCharacter(str) {
   if (str === undefined) throw new Error("str is required");
-  // Add your code here!
+  else if (str.length % 2 === 0) {
+    let mid = str.length / 2
+    return str.slice(mid - 1, mid + 1)
+  } else {
+    let newLength = str.length + 1
+    mid = newLength / 2
+    return str.slice(mid - 1, mid)
+  }
 }
 
 function reverseWord(word) {
