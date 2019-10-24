33aa5f712a7f467790d3eaf2fa73c80b4a8cae2c
Kate Santo <katerine.santo@gmail.com>
2019-10-24 23:33:40 +0100

    Week 4 challenges done! :D

--- challenges/week4.js
+++ challenges/week4.js
@@ -13,45 +13,81 @@ function findSmallNums(nums) {
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-const correctNamesOnly = names.filter(function (name) {
-  if (name[0] === char) {
-    return true
-  } else {
-    return false;
-  }
-})
-return correctNamesOnly;
+  const correctNamesOnly = names.filter(function (name) {
+    if (name[0] === char) {
+      return true
+    } else {
+      return false;
+    }
+  })
+  return correctNamesOnly;
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-  // Your code here
+  const verbsOnly = words.filter(function (word) {
+    if (word.substr(0, 3) === "to ") {
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return verbsOnly;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  const integersOnly = nums.filter(function (num) {
+    if (Number.isInteger(num)) {
+      return true
+    } else {
+      return false
+    }
+  })
+  return integersOnly
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-  // Your code here
+  let citiesOnly = []
+  users.forEach(function (user) {
+    citiesOnly.push(user.data.city.displayName)
+  })
+  return citiesOnly
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  const squareRoots = nums.map(function (num) {
+    let squared = Math.sqrt(num)
+    let toTwoDecimals = Math.round(squared * 100) / 100
+    return toTwoDecimals
+  })
+  return squareRoots
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  const relevantSentencesOnly = sentences.filter(function (sentence) {
+    if (sentence.toLowerCase().includes(str.toLowerCase()) === true) {
+      return true;
+    } else {
+      return false;
+    }
+  })
+  return relevantSentencesOnly
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+  const newArr = triangles.map(function (item) {
+    let sortedTriangle = item.sort(function (a, b) { return a - b });
+    let longestSide = sortedTriangle.pop()
+    return longestSide
+  })
+  return newArr
 }
 
 module.exports = {
