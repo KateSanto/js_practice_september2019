78b72bbcc66ff2f5c4dd723ae8edc23173a451c8
Kate Santo <katerine.santo@gmail.com>
2019-11-19 17:35:06 +0000

    Tidying up week4 file as it had some weird formatting that was failing tests

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,6 +1,5 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-<<<<<<< HEAD
   const smallNumsOnly = nums.filter(function (num) {
     if (num < 1) {
       return true;
@@ -9,15 +8,12 @@ function findSmallNums(nums) {
     }
   });
   return smallNumsOnly;
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-<<<<<<< HEAD
   const correctNamesOnly = names.filter(function (name) {
     if (name[0] === char) {
       return true
@@ -26,14 +22,11 @@ function findNamesBeginningWith(names, char) {
     }
   })
   return correctNamesOnly;
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function findVerbs(words) {
   if (!words) throw new Error("words is required");
-<<<<<<< HEAD
   const verbsOnly = words.filter(function (word) {
     if (word.substr(0, 3) === "to ") {
       return true;
@@ -42,14 +35,11 @@ function findVerbs(words) {
     }
   })
   return verbsOnly;
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-<<<<<<< HEAD
 
   const integersOnly = nums.filter(function (num) {
     if (Number.isInteger(num)) {
@@ -59,42 +49,33 @@ function getIntegers(nums) {
     }
   })
   return integersOnly
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function getCities(users) {
   if (!users) throw new Error("users is required");
-<<<<<<< HEAD
   let citiesOnly = []
   users.forEach(function (user) {
     citiesOnly.push(user.data.city.displayName)
   })
   return citiesOnly
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function getSquareRoots(nums) {
   if (!nums) throw new Error("nums is required");
-<<<<<<< HEAD
   const squareRoots = nums.map(function (num) {
     let squared = Math.sqrt(num)
     let toTwoDecimals = Math.round(squared * 100) / 100
     return toTwoDecimals
   })
   return squareRoots
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function findSentencesContaining(sentences, str) {
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-<<<<<<< HEAD
   const relevantSentencesOnly = sentences.filter(function (sentence) {
     if (sentence.toLowerCase().includes(str.toLowerCase()) === true) {
       return true;
@@ -103,23 +84,18 @@ function findSentencesContaining(sentences, str) {
     }
   })
   return relevantSentencesOnly
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 function getLongestSides(triangles) {
   if (!triangles) throw new Error("triangles is required");
-<<<<<<< HEAD
   const newArr = triangles.map(function (item) {
     let sortedTriangle = item.sort(function (a, b) { return a - b });
     let longestSide = sortedTriangle.pop()
     return longestSide
   })
   return newArr
-=======
   // Your code here
->>>>>>> 3157770dfa3af9a26c0e811f43025a4147af1173
 }
 
 module.exports = {
